9c85620d71e4281f0a8f95cfb5f0735dd4cb72bf
mashiike <ikeda-masashi@kayac.com>
2024-09-30 17:51:51 +0900

    fix

--- otlp/json.go
+++ otlp/json.go
@@ -106,39 +106,22 @@ func convertTraceIDAndSpanIDBase64ToHexForAny(data any) any {
 
 // keyIsTraceIDOrSpanID checks if the key is traceID or spanID.
 // return hexBytes, base64Bytes, isTraceIDOrSpanID
-func keyIsTraceIDOrSpanID(k string) (int, int, bool) {
+func keyIsTraceIDOrSpanID(k string) bool {
 	key := strings.ReplaceAll(k, "_", "")
 	key = strings.ToLower(key)
-	if strings.Contains(key, "traceid") {
-		// traceID is 16 bytes
-		return 16, 24, true
-	}
-	if strings.Contains(key, "spanid") {
-		// spanID is 8 bytes
-		return 8, 12, true
-	}
-	return 0, 0, false
+	return strings.Contains(key, "traceid") || strings.Contains(key, "spanid")
 }
 
 func convertTraceIDAndSpanIDBase64ToHexForMap(data map[string]interface{}) map[string]interface{} {
 	for k, v := range data {
-		if hexBytes, base64Bytes, isID := keyIsTraceIDOrSpanID(k); isID {
+		if keyIsTraceIDOrSpanID(k) {
 			if s, ok := v.(string); ok {
 				bs, err := base64.StdEncoding.DecodeString(s)
 				if err != nil {
 					slog.Warn("failed to convert traceID and spanID from base64 to hex", "key", k, "error", err.Error())
 					continue
 				}
-				if len(bs) != base64Bytes {
-					slog.Warn("unexpected length of traceID and spanID", "key", k, "base64_length", len(bs), "expected_length", base64Bytes)
-					continue
-				}
-				converted := strings.ToUpper(hex.EncodeToString(bs))
-				if len(converted) != hexBytes {
-					slog.Warn("unexpected length of traceID and spanID", "key", k, "hex_length", len(converted), "expected_length", hexBytes)
-					continue
-				}
-				data[k] = converted
+				data[k] = strings.ToUpper(hex.EncodeToString(bs))
 				continue
 			}
 			slog.Warn("unexpected type of traceID and spanID", "key", k, "value_type", fmt.Sprintf("%T", v))
@@ -205,23 +188,14 @@ func convertTraceIDAndSpanIDHexToBase64ForAny(data any) any {
 
 func convertTraceIDAndSpanIDHexToBase64ForMap(data map[string]interface{}) map[string]interface{} {
 	for k, v := range data {
-		if hexBytes, base64Bytes, isID := keyIsTraceIDOrSpanID(k); isID {
+		if keyIsTraceIDOrSpanID(k) {
 			if s, ok := v.(string); ok {
 				bs, err := hex.DecodeString(s)
 				if err != nil {
 					slog.Warn("failed to convert traceID and spanID from hex to base64", "error", err.Error())
 					continue
 				}
-				if len(bs) != hexBytes {
-					slog.Warn("unexpected length of traceID and spanID", "key", k, "hex_length", len(bs), "expected_length", hexBytes)
-					continue
-				}
-				converted := base64.StdEncoding.EncodeToString(bs)
-				if len(converted) != base64Bytes {
-					slog.Warn("unexpected length of traceID and spanID", "key", k, "base64_length", len(converted), "expected_length", base64Bytes)
-					continue
-				}
-				data[k] = converted
+				data[k] = base64.StdEncoding.EncodeToString(bs)
 				continue
 			}
 			slog.Warn("unexpected type of traceID and spanID", "key", k, "value_type", fmt.Sprintf("%T", v))
