0174182089562a8b697339853d499537edc46bff
mashiike <ikeda-masashi@kayac.com>
2024-09-30 17:48:14 +0900

    fix indent marshal

--- otlp/json.go
+++ otlp/json.go
@@ -24,7 +24,7 @@ func MarshalJSON(msg proto.Message) ([]byte, error) {
 	if err != nil {
 		return nil, err
 	}
-	return convertTraceIDAndSpanIDBase64ToHex(data), nil
+	return convertTraceIDAndSpanIDBase64ToHex(data, ""), nil
 }
 
 // MarshalIndentJSON marshals a proto.Message to indented JSON bytes. for OTLP, traceID and spanID are converted from base64 to hex.
@@ -36,12 +36,13 @@ func MarshalIndentJSON(msg proto.Message, indent string) ([]byte, error) {
 	if err != nil {
 		return nil, err
 	}
-	return convertTraceIDAndSpanIDBase64ToHex(data), nil
+	return convertTraceIDAndSpanIDBase64ToHex(data, indent), nil
 }
 
 type JSONEncoder struct {
 	writer    io.Writer
 	marshaler protojson.MarshalOptions
+	indent    string
 }
 
 func NewJSONEncoder(writer io.Writer) *JSONEncoder {
@@ -54,6 +55,7 @@ func NewJSONEncoder(writer io.Writer) *JSONEncoder {
 func (e *JSONEncoder) SetIndent(indent string) {
 	e.marshaler.Multiline = true
 	e.marshaler.Indent = indent
+	e.indent = indent
 }
 
 func (e *JSONEncoder) Encode(msg proto.Message) error {
@@ -62,18 +64,26 @@ func (e *JSONEncoder) Encode(msg proto.Message) error {
 		return err
 	}
 
-	data = convertTraceIDAndSpanIDBase64ToHex(data)
+	data = convertTraceIDAndSpanIDBase64ToHex(data, e.indent)
 	_, err = e.writer.Write(data)
 	return err
 }
 
-func convertTraceIDAndSpanIDBase64ToHex(data []byte) []byte {
+func convertTraceIDAndSpanIDBase64ToHex(data []byte, indent string) []byte {
 	var m any
 	if err := json.Unmarshal(data, &m); err != nil {
 		slog.Warn("failed to convert traceID and spanID from base64 to hex", "error", err.Error())
 		return data
 	}
 	m = convertTraceIDAndSpanIDBase64ToHexForAny(m)
+	if indent != "" {
+		converted, err := json.MarshalIndent(m, "", indent)
+		if err != nil {
+			slog.Warn("failed to convert traceID and spanID from base64 to hex", "error", err.Error())
+			return data
+		}
+		return converted
+	}
 	converted, err := json.Marshal(m)
 	if err != nil {
 		slog.Warn("failed to convert traceID and spanID from base64 to hex", "error", err.Error())
@@ -94,22 +104,41 @@ func convertTraceIDAndSpanIDBase64ToHexForAny(data any) any {
 	return data
 }
 
-func keyIsTraceIDOrSpanID(k string) bool {
+// keyIsTraceIDOrSpanID checks if the key is traceID or spanID.
+// return hexBytes, base64Bytes, isTraceIDOrSpanID
+func keyIsTraceIDOrSpanID(k string) (int, int, bool) {
 	key := strings.ReplaceAll(k, "_", "")
 	key = strings.ToLower(key)
-	return strings.Contains(key, "traceid") || strings.Contains(key, "spanid")
+	if strings.Contains(key, "traceid") {
+		// traceID is 16 bytes
+		return 16, 24, true
+	}
+	if strings.Contains(key, "spanid") {
+		// spanID is 8 bytes
+		return 8, 12, true
+	}
+	return 0, 0, false
 }
 
 func convertTraceIDAndSpanIDBase64ToHexForMap(data map[string]interface{}) map[string]interface{} {
 	for k, v := range data {
-		if keyIsTraceIDOrSpanID(k) {
+		if hexBytes, base64Bytes, isID := keyIsTraceIDOrSpanID(k); isID {
 			if s, ok := v.(string); ok {
 				bs, err := base64.StdEncoding.DecodeString(s)
 				if err != nil {
-					slog.Warn("failed to convert traceID and spanID from base64 to hex", "error", err.Error())
-				} else {
-					data[k] = strings.ToUpper(hex.EncodeToString(bs))
+					slog.Warn("failed to convert traceID and spanID from base64 to hex", "key", k, "error", err.Error())
+					continue
 				}
+				if len(bs) != base64Bytes {
+					slog.Warn("unexpected length of traceID and spanID", "key", k, "base64_length", len(bs), "expected_length", base64Bytes)
+					continue
+				}
+				converted := strings.ToUpper(hex.EncodeToString(bs))
+				if len(converted) != hexBytes {
+					slog.Warn("unexpected length of traceID and spanID", "key", k, "hex_length", len(converted), "expected_length", hexBytes)
+					continue
+				}
+				data[k] = converted
 				continue
 			}
 			slog.Warn("unexpected type of traceID and spanID", "key", k, "value_type", fmt.Sprintf("%T", v))
@@ -176,14 +205,23 @@ func convertTraceIDAndSpanIDHexToBase64ForAny(data any) any {
 
 func convertTraceIDAndSpanIDHexToBase64ForMap(data map[string]interface{}) map[string]interface{} {
 	for k, v := range data {
-		if keyIsTraceIDOrSpanID(k) {
+		if hexBytes, base64Bytes, isID := keyIsTraceIDOrSpanID(k); isID {
 			if s, ok := v.(string); ok {
 				bs, err := hex.DecodeString(s)
 				if err != nil {
 					slog.Warn("failed to convert traceID and spanID from hex to base64", "error", err.Error())
-				} else {
-					data[k] = base64.StdEncoding.EncodeToString(bs)
+					continue
 				}
+				if len(bs) != hexBytes {
+					slog.Warn("unexpected length of traceID and spanID", "key", k, "hex_length", len(bs), "expected_length", hexBytes)
+					continue
+				}
+				converted := base64.StdEncoding.EncodeToString(bs)
+				if len(converted) != base64Bytes {
+					slog.Warn("unexpected length of traceID and spanID", "key", k, "base64_length", len(converted), "expected_length", base64Bytes)
+					continue
+				}
+				data[k] = converted
 				continue
 			}
 			slog.Warn("unexpected type of traceID and spanID", "key", k, "value_type", fmt.Sprintf("%T", v))
